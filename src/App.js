06206ed23aa539cdadcdfa038d8ab5b4c08e5561
goodchuck <y502100@naver.com>
2021-10-18 20:49:27 +0900

    21/10/18 3.4.2.2 p112 useState 사용하기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Component } from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
+import Say from './Say';
 
 // function App() {
 //   return <MyComponent name="react" favoriteNumber={1}>리액트</MyComponent>;
 // }
 
 const App = () => {
-  return <Counter />;
+  return <Say />;
 };
 export default App;
