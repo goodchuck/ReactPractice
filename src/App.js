802a97298f3c500c6099fcbb11ab37075931a57f
goodchuck <y502100@naver.com>
2021-10-18 20:28:00 +0900

    21/10/18 3.3.7 클래스형 컴포넌트에서 props 사용하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,6 @@ import { Component } from 'react';
 import MyComponent from './MyComponent';
 
 function App() {
-  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+  return <MyComponent name="react" favoriteNumber={1}>리액트</MyComponent>;
 }
 export default App;
