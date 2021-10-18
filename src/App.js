373d6ea4788ce3db265fb287857a62a0736adce9
goodchuck <y502100@naver.com>
2021-10-18 20:21:26 +0900

    21/10/18 p99 3.3.6.1 isRequired를 사용하여 필수 propTypes 설정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,6 @@ import { Component } from 'react';
 import MyComponent from './MyComponent';
 
 function App() {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
 }
 export default App;
