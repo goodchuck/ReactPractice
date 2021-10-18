2c07a1ac7ced1eafe2bf35ea11497b224e89c3b1
goodchuck <y502100@naver.com>
2021-10-18 20:35:06 +0900

    21/10/18 p104 3.4.1 클래스형 컴포넌트의 state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
-function App() {
-  return <MyComponent name="react" favoriteNumber={1}>리액트</MyComponent>;
-}
+// function App() {
+//   return <MyComponent name="react" favoriteNumber={1}>리액트</MyComponent>;
+// }
+
+const App = () => {
+  return <Counter />;
+};
 export default App;
