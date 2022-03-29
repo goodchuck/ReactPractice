6d80df3f0b19db9450362ac9d78799f08a9128c6
goodchuck <y502100@naver.com>
2022-03-29 17:15:30 +0900

    22/03/29 p178 라이프사이클 메서드 사용하기

--- src/App.js
+++ src/App.js
@@ -8,15 +8,35 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
 
 // function App() {
 //   return <MyComponent name="react" favoriteNumber={1}>리액트</MyComponent>;
 // }
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample/>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
     );
   }
 }
