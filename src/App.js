4451ab05d7a341b8cf64b9d5999b99aae6ef3bd2
goodchuck <y502100@naver.com>
2022-03-30 16:49:54 +0900

    22/03/30 p223 Sass 사용하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import Counter from './Counter';
 import Info from './Info';
 import Average from './Average';
+import SassComponent from './SassComponent';
 
-const App = () => {
-  // const [visible, setVisible] = useState(false);
-  // return (
-  //   <div>
-  //     <button
-  //       onClick={() => {
-  //         setVisible(!visible);
-  //       }}
-  //     >
-  //       {visible ? '숨기기' : '보이기'}
-  //     </button>
-  //     <hr />
-  //     {visible && <Info />}
-  //   </div>
-  // );
-  return <Average />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    )
+  }
+}
+
+// const App = () => {
+//   // const [visible, setVisible] = useState(false);
+//   // return (
+//   //   <div>
+//   //     <button
+//   //       onClick={() => {
+//   //         setVisible(!visible);
+//   //       }}
+//   //     >
+//   //       {visible ? '숨기기' : '보이기'}
+//   //     </button>
+//   //     <hr />
+//   //     {visible && <Info />}
+//   //   </div>
+//   // );
+//   return <Average />;
+// };
 
 export default App;
