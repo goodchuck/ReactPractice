35532909dc09817a1f806bd3fe3a692f3bfcb64a
goodchuck <y502100@naver.com>
2022-03-30 15:16:19 +0900

    22/03/30 p200 useReducer 카운터 구현하기

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import Counter from './Counter';
 import Info from './Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? '숨기기' : '보이기'}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  return <Counter />;
 };
 
 export default App;
